e2169c5cd21247c299b551fd14ca30594e5b6c58
Anthonyk95 <ankrits@teiemt.gr>
2018-05-04 04:00:19 +0300

    registration under construction

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+// Adding Registration'
+var registerRoute = require ('./routes/register')
 
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -21,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/re',registerRoute)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
